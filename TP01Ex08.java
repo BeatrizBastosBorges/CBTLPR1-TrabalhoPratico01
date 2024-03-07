6c5381197fda370e9b7437ac212b79383ac7ff1e
l3gium <131935219+l3gium@users.noreply.github.com>
2024-03-07 11:18:53 -0300

    Update TP01Ex08.java

--- TP01Ex08.java
+++ TP01Ex08.java
@@ -5,7 +5,9 @@ public class TP01Ex08 {
 
     /* 8. Sabendo que uma milha marítima equivale a um mil, oitocentos e cinquenta e
     dois metros e que um quilômetro possui mil metros, fazer um programa para
-    converter milhas marítimas em quilômetros. */
+    converter milhas marítimas em quilômetros. 
+    Desenvolvedores: Beatriz Bastos Borges e Miguel Luizatto Alves
+    */
 
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
