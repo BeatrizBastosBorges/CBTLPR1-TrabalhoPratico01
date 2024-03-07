d96cf978090554eb5f49aa39f843006b787703de
l3gium <131935219+l3gium@users.noreply.github.com>
2024-03-07 11:19:09 -0300

    Update TP01Ex09.java

--- TP01Ex09.java
+++ TP01Ex09.java
@@ -5,7 +5,9 @@ import java.lang.Math;
 public class TP01Ex09 {
 
     /* 9. Calcular e exibir a tensão de um determinado circuito eletrônico a partir dos
-    alores da resistência e corrente elétrica que serão digitados. Utilize a lei de Ohm. */
+    alores da resistência e corrente elétrica que serão digitados. Utilize a lei de Ohm. 
+    Desenvolvedores: Beatriz Bastos Borges e Miguel Luizatto Alves
+    */
 
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
